42b8d5f172306af2378462d20a6dc51e0ea944f6
Harish Nigam <harrysingh17022552@gmail.com>
2025-11-05 05:15:24 +0530

    Creating Route Files for all component that will be created in future

---
+++ src/assets/data.js
@@ -0,0 +1,23 @@
+export const data = () => {
+  const list = [
+    {
+      id: 1,
+      task: "Assignment1",
+      description: "Complete Module 3 Assignment",
+      status: true,
+    },
+    {
+      id: 2,
+      task: "Assignment2",
+      description: "Complete Module 3 Assignment",
+      status: false,
+    },
+    {
+      id: 3,
+      task: "Assignment3",
+      description: "Complete Final React Assignment",
+      status: false,
+    },
+  ];
+  return list;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,30 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import About from "./dummy_component/About.jsx";
+import Contact from "./dummy_component/Contact.jsx";
+import E404 from "./dummy_component/E404.jsx";
+import ToDoList from "./component/ToDoList";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+function RootProvider() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      children: [
+        { path: "/", element: <ToDoList /> },
+        { path: "/contact", element: <Contact /> },
+        { path: "/about", element: <About /> },
+      ],
+      element: <App />,
+      ErrorBoundary: E404,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RootProvider />
   </StrictMode>
 );
