7faea1987a209be814bdf6247ac9b88c714e6e49
Harish Nigam <harrysingh17022552@gmail.com>
2025-11-05 05:18:22 +0530

    Creating Header for App and updating APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./Header";
 function App() {
-  <div>App</div>;
+  const [user, setUSer] = useState({ fname: "Harish", lname: "Nigam" });
+  return (
+    <main className="flex flex-col gap-8 w-full h-screen overflow-scroll noscrollbar">
+      <Header user={user} />
+      <Outlet/>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+export default function Header({ user }) {
+  return (
+    <header className="w-full bg-black flex gap-4 p-4 items-center justify-between overflow-scroll whitespace-nowrap noscrollbar">
+      <div className="flex gap-8 items-center">
+        <Link to="/" className="font-bold">
+          Home
+        </Link>
+        <Link to="/about" className="font-bold">
+          About Us
+        </Link>
+        <Link to="/contact" className="font-bold">
+          Contact
+        </Link>
+      </div>
+      <div className="flex gap-2 items-center">
+        <p className="font-bold">
+          {user.fname} {user.lname}
+        </p>
+        <div className="p-2 bg-gray-700 rounded-full font-bold">
+          {user.fname[0]}
+          {user.lname[0]}
+        </div>
+      </div>
+    </header>
+  );
+}
